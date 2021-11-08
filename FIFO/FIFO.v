e9944e7e874af37fc47b9eb7aefd3b1fbd4191fc
fabio0610 <fabio0610@hotmail.com>
2021-11-07 21:03:36 -0600

    se agrega FiFo no final

---
+++ FIFO/FIFO.v
@@ -0,0 +1,122 @@
+`include "true_dpram_sclk.v"
+
+module FIFO (
+    input clk,                      
+    input push,
+    input pop,
+    input [2:0] umbral_superior,            
+    input [2:0] umbral_inferior,
+    input [3:0] state, 
+    input [9:0] data_in,          
+    output [9:0] data_out,
+    output reg almost_full,
+    output reg almost_empty, empty
+);
+
+reg [2:0] wr_ptr, rd_ptr;
+reg [2:0] low_space, much_space;
+reg we_a, re_a;
+reg [3:0] contador;
+reg full, empty_aux;
+
+true_dpram_sclk memory (
+                .data_a(data_in),
+                .addr_wa(wr_ptr),
+                .addr_ra(rd_ptr),
+                .clk(clk),
+                .we_a(we_a),
+                .re_a(re_a),
+                .q_a(data_out),
+                .state(state)
+);
+always@(posedge clk)begin
+    if(state == 4'b0001)begin 
+        low_space <= 0;
+        much_space <= 0;
+    end  
+    if(state == 4'b0010)begin
+        low_space <= umbral_superior;
+        much_space <= umbral_inferior;
+    end
+end
+
+always@(posedge clk)begin
+    if(state == 4'b0001)begin
+        wr_ptr <= 3'b0;
+        rd_ptr <= 3'b0;
+        contador <= 3'b0;
+        empty = 1;
+    end
+    else begin
+        
+        if((push == 1) & (contador != 4'b1000) & (data_in != 10'b0))begin
+            wr_ptr <= wr_ptr + 1; 
+            contador <= contador + 1;
+            empty <= 0;
+      
+        if(contador == 4'b1000) begin
+            full <= 1;
+        end
+        end
+
+        
+        if((pop == 1)  & (contador != 4'b0000))begin
+            rd_ptr <= rd_ptr + 1; 
+            contador <= contador - 1;
+            full <= 0;
+      
+            if(contador == 4'b0001)begin
+                empty <= 1;
+            end
+        end
+        
+    end
+end
+
+always@(*)begin
+    
+    if(state == 4'b0001)begin
+        we_a <= 0;
+        re_a <= 0;
+        
+    end
+    else begin
+        if((push == 1) & (contador != 4'b1000))begin
+            we_a = 1;
+        end
+        else if(we_a <= 1)begin
+            we_a = 0;
+        end
+        if((pop == 1) & (contador != 4'b0000))begin
+            re_a = 1;
+        end
+        else if(re_a <= 1)begin
+            re_a = 0;
+        end
+    end
+end
+
+always@(*)begin
+
+    if(state==4'b0001)begin
+        almost_full = 0;
+        almost_empty = 0;
+    end
+    else begin
+        
+        if(contador >= low_space)begin
+            almost_full = 1;
+        end
+        else begin
+            almost_full = 0;
+        end
+        if(contador <= much_space)begin
+            almost_empty = 1;
+        end
+        else begin
+            almost_empty = 0;
+        end
+    end
+end
+
+endmodule
