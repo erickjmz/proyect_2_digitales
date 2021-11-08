c601315c76eea9a6b730111285fb5b8c2de3e8b9
Gabo4x <gabo040198@gmail.com>
2021-11-08 10:07:35 -0600

    Pruebas a Memoria RAM

--- Memoria_RAM/Banco_pruebas.v
+++ Memoria_RAM/Banco_pruebas.v
@@ -1,6 +1,8 @@
 `timescale 	1s				/ 100ps
 `include "true_dpram_sclk.v"
 `include "Probador.v"
+`include "Memoria_synth.v"
+`include "cmos_cells.v"
 
 module Banco_pruebas;
 	wire [9:0] data_a, q_a;
@@ -19,6 +21,18 @@ module Banco_pruebas;
 					       .re_a		(re_a),
 					       .clk		(clk),
 					       .state		(state[3:0]));
+
+	Memoria_synth Memoria_RAM_Estructural( /*AUTOINST*/
+					     // Outputs
+					     .q_a		(q_a[9:0]),
+					     // Inputs
+					     .addr_ra		(addr_ra[2:0]),
+					     .addr_wa		(addr_wa[2:0]),
+					     .clk		(clk),
+					     .data_a		(data_a[9:0]),
+					     .re_a		(re_a),
+					     .state		(state[3:0]),
+					     .we_a		(we_a));
 						     
 	Probador Probador( /*AUTOINST*/
 			  // Outputs
