589c3b72aa3f2f35e2a5caff4fd848996b9db176
erickjmz <erjomuza@gmail.com>
2021-11-26 09:51:29 -0800

    fix

--- MUX/mux.v
+++ MUX/mux.v
@@ -9,21 +9,21 @@ module mux
 	always @ (*)
 	begin
 		if (reset_L == 0) begin
-			data_out <= 0;
+			data_out = 0;
 		end else begin
 			if (data_in0 != 0 && data_in0[11:10] == 0) begin
-				data_out <= data_in0;
+				data_out = data_in0;
 			end else
 			if (data_in1 != 0 && data_in1[11:10] == 1) begin
-				data_out <= data_in1;
+				data_out = data_in1;
 
 			end else
 			if (data_in2 != 0 && data_in2[11:10] == 2) begin
-				data_out <= data_in2;
+				data_out = data_in2;
 
 			end else
 			if (data_in3 != 0 && data_in3[11:10] == 3) begin
-				data_out <= data_in3;
+				data_out = data_in3;
 			end
 		end
 	end
