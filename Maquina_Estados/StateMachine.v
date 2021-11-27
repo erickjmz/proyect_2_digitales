11bb3a35dceba97ade492e28d863a1574482a628
erickjmz <erjomuza@gmail.com>
2021-11-26 20:36:47 -0800

    reset_l

--- Maquina_Estados/StateMachine.v
+++ Maquina_Estados/StateMachine.v
@@ -5,7 +5,8 @@ module StateMachine (
     input [2:0] High_Threshold, Low_Threshold,
     input [7:0] empties,
     output reg [2:0] sup_Threshold, inf_Threshold,
-    output reg [3:0] state
+    output reg [3:0] state,
+    output reg reset_L
 );
 reg [3:0] ProximoEstado, Estado;
 
@@ -16,12 +17,14 @@ parameter [3:0] ACTIVE = 4'b1000;
 
 always@(posedge clk)begin
     if (reset)begin
+        reset_L <= 0;
         Estado <= RESET;
         sup_Threshold<=0;
         inf_Threshold<=0;
     end
     else begin
         if (init)begin
+            reset_L <= 1;
             Estado <= INIT;
             sup_Threshold <= High_Threshold;
             inf_Threshold <= Low_Threshold;
@@ -33,24 +36,25 @@ always@(posedge clk)begin
 end
 
 always@(*)begin
-    
+
     if(reset)begin
         state = RESET;
         ProximoEstado = 0;
+        reset_L = 0;
     end
     else begin
     state = Estado;
     ProximoEstado = Estado;
     case(Estado)
-        
+
         RESET: begin
             ProximoEstado = INIT;
         end
-        
+
         INIT: begin
             ProximoEstado = IDLE;
         end
-        
+
         IDLE: begin
             if(empties != 8'hFF)begin
                 ProximoEstado = ACTIVE;
@@ -59,7 +63,7 @@ always@(*)begin
                 ProximoEstado = IDLE;
             end
         end
-        
+
         ACTIVE: begin
             if(empties != 8'hFF)begin
                 ProximoEstado = ACTIVE;
